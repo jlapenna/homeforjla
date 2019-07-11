888775aa326e4ff81988297048d0cebd2961f7ea
Joe LaPenna <jlapenna@gmail.com>
2019-07-10 19:32:02 -0700

    Fix push/popping git directory.

--- raspberrypi/install_assistant_relay.sh
+++ raspberrypi/install_assistant_relay.sh
@@ -15,32 +15,34 @@ function main() {
   local config_path="${configurations_path}/config.json"
   local key_file_path="${configurations_path}/secrets/homeforjla.json"
   local saved_tokens_path="${configurations_path}/tokens/homeforjla-tokens.json"
-  server/configurations/tokens/homeforjla-tokens.json
 
   pushd $install_path
 
   if [[ ! -d "assistant-relay" ]]; then
     echo "Cloning and updating repo."
     git clone https://github.com/greghesp/assistant-relay.git
-    cd assistant-relay
+    pushd assistant-relay
     git checkout -b production
     sed -i 's/"google-assistant": "^0.2.0"/"google-assistant": "^0.5.4"/g' package.json
     npm install
     npm audit fix
     git commit -a -m'Update google-assistant configuration'
-
-    cp ${repo_path}/private/assistant_relay_client_secret.json ${key_file_path}
-    cp ${repo_path}/raspberrypi/assistant_relay_config.json ${config_path} 
-    sed \
-        -e "s#INSTALL_PATH#${install_path}#g" \
-        -i ${config_path};
-    git commit -a -m'Update configuration and client secrets.'
   else
     echo "Pulling and rebasing repo."
+    pushd assistant-relay
     git pull
     git rebase master
   fi
 
+  echo "Copying secrets and configs."
+  cp ${repo_path}/private/assistant_relay_client_secret.json ${key_file_path}
+  cp ${repo_path}/raspberrypi/assistant_relay_config.json ${config_path} 
+  sed \
+      -e "s#INSTALL_PATH#${install_path}#g" \
+      -i ${config_path};
+  git commit -a -m'Update configuration and client secrets.'
+
+  popd
   popd
 }
 
