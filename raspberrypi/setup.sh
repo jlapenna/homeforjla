9fe911e1cb5bc6e8bf1a7d2dae3f7a07d186699d
Joe LaPenna <jlapenna@gmail.com>
2019-07-25 20:46:19 -0700

    Add setup.sh script for initial setup of a pi.

---
+++ raspberrypi/setup.sh
@@ -0,0 +1,8 @@
+echo "Setting up git"
+sudo apt-get install -y git
+git config --global user.email jlapenna@gmail.com;
+git config --global user.name "Joe LaPenna";
+
+echo "Installing nodejs 12"
+curl -sL https://deb.nodesource.com/setup_12.x | bash -
+apt-get install -y nodejs
